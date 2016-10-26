b2edd8a23beacfd3a6d6d3537f10d84d07a60583
sanket226 <khambete.sanket@zalora.com>
2016-10-26 13:37:52 +0800

    create initial version of employee directory

---
+++ Employee-Directory/js/app.js
@@ -0,0 +1,40 @@
+var Header = React.createClass({
+    render: function () {
+        return (
+            <h1 className="title">{this.props.text}</h1>
+        );
+    }
+});
+
+var SearchBar = React.createClass({
+    render: function() {
+        return (
+            <input type="search"/>
+        );
+    }
+});
+
+var EmployeeList = React.createClass({
+    render: function() {
+        return (
+            <ul>
+                <li>Chris</li>
+                <li>Lisa</li>
+            </ul>
+        );
+    }
+});
+
+var HomePage = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Header text="Employee Directory"/>
+                <SearchBar />
+                <EmployeeList />
+            </div>
+        );
+    }
+});
+
+React.render(<HomePage />, document.body);
